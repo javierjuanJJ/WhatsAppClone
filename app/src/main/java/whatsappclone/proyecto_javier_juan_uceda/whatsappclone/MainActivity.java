4fd722f1c614be03305a421aa858a4b75f74cd6f
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-10-03 11:28:31 +0200

    Commit 2: Phone Number Authentication

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappclone/MainActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappclone/MainActivity.java
@@ -1,14 +1,123 @@
 package whatsappclone.proyecto_javier_juan_uceda.whatsappclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText phoneNumber, code;
+    private Button send;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private String mVerificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUI();
+
+    }
+
+    private void setUI() {
+
+        FirebaseApp.initializeApp(this);
+
+        usersInLoggedIn();
+
+        phoneNumber = findViewById(R.id.phoneNumber);
+        code = findViewById(R.id.code);
+        send = findViewById(R.id.send);
+
+        send.setOnClickListener(this);
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(verificationId, forceResendingToken);
+                mVerificationId = verificationId;
+                code.setText(getString(R.string.btnVerifyNumber));
+
+
+            }
+        };
+
+    }
+
+    private void verifyPhoneNumberWithCode() {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code.getText().toString());
+        signInWithPhoneCredential(credential);
+    }
+
+    private void signInWithPhoneCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    usersInLoggedIn();
+                }
+            }
+        });
+    }
+
+    private void usersInLoggedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            startActivity(new Intent(this, MainPageActivity.class));
+            finish();
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.send:
+                if (mVerificationId != null) {
+                    verifyPhoneNumberWithCode();
+                } else {
+                    startPhoneNumberVerification();
+                }
+
+                break;
+        }
+    }
+
+    private void startPhoneNumberVerification() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks
+        );
     }
 }
---
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappclone/MainPageActivity.java
@@ -0,0 +1,42 @@
+package whatsappclone.proyecto_javier_juan_uceda.whatsappclone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        setUI();
+
+    }
+
+    private void setUI() {
+        btnLogOut = findViewById(R.id.logout);
+        btnLogOut.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+}
