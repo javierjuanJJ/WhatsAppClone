e975c9f937f8ff1a1a04f41e94e92d759c17db15
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-10-06 15:45:44 +0200

    Commit 13: Sending Notifications to Chat Users

--- app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappclone/MainPageActivity.java
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappclone/MainPageActivity.java
@@ -4,10 +4,12 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,15 +21,24 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.onesignal.OSDeviceState;
+import com.onesignal.OSSubscriptionObserver;
+import com.onesignal.OSSubscriptionStateChanges;
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
 import whatsappclone.proyecto_javier_juan_uceda.whatsappclone.Chat.ChatListAdapter;
 import whatsappclone.proyecto_javier_juan_uceda.whatsappclone.Chat.ChatObject;
+import whatsappclone.proyecto_javier_juan_uceda.whatsappclone.Utils.SendNotification;
 
-public class MainPageActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainPageActivity extends AppCompatActivity implements View.OnClickListener, OSSubscriptionObserver {
 
     private Button btnLogOut, btnFindUser;
+    private static final String ONESIGNAL_APP_ID = "d01f2fac-0fe4-40ee-96b1-9ca107d94da2";
+    //private static final String ONESIGNAL_APP_ID = "146ff33d-e6e5-474b-b688-416ad622c1b2";
 
     private RecyclerView mChatList;
     private RecyclerView.Adapter mChatListAdapter;
@@ -39,6 +50,36 @@ public class MainPageActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        OSDeviceState deviceState = OneSignal.getDeviceState();
+        String userId = deviceState != null ? deviceState.getUserId() : null;
+
+        FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("notificationKey").setValue(userId);
+
+        OneSignal.unsubscribeWhenNotificationsAreDisabled(true);
+        OneSignal.pauseInAppMessages(true);
+        OneSignal.setLocationShared(false);
+
+        //OneSignal.addSubscriptionObserver(this);
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications();
+
+        try {
+            //SendNotification.SendNotification("message 1", "heading 1", ONESIGNAL_APP_ID);
+            SendNotification.SendNotification("message 1", "heading 1", userId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         setUI();
 
@@ -70,6 +111,7 @@ public class MainPageActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.logout:
+                OneSignal.disablePush(true);
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(this, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -121,5 +163,19 @@ public class MainPageActivity extends AppCompatActivity implements View.OnClickL
         mChatList.setLayoutManager(mChatListLayoutManager);
         mChatListAdapter = new ChatListAdapter(chatList);
         mChatList.setAdapter(mChatListAdapter);
+    }
+
+    @Override
+    public void onOSSubscriptionChanged(OSSubscriptionStateChanges osSubscriptionStateChanges) {
+        if (!osSubscriptionStateChanges.getFrom().isSubscribed() &&
+                osSubscriptionStateChanges.getTo().isSubscribed()) {
+            new AlertDialog.Builder(this)
+                    .setMessage("You've successfully subscribed to push notifications!")
+                    .show();
+            // get player ID
+            osSubscriptionStateChanges.getTo().getUserId();
+        }
+
+        Log.i("Debug", "onOSSubscriptionChanged: " + osSubscriptionStateChanges);
     }
 }
---
+++ app/src/main/java/whatsappclone/proyecto_javier_juan_uceda/whatsappclone/Utils/SendNotification.java
@@ -0,0 +1,31 @@
+package whatsappclone.proyecto_javier_juan_uceda.whatsappclone.Utils;
+
+import android.util.Log;
+
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SendNotification {
+    public static void SendNotification(String message, String receiver, String notificationKey) throws JSONException {
+        Log.i("OneSignalExample", "notificationKey: " + notificationKey);
+        JSONObject jsonObject = new JSONObject("{'contents': {'en':'Test Message'}, 'include_player_ids': ['" + notificationKey + "']}");
+
+        //JSONObject jsonObject = new JSONObject("{ 'app_id':" + notificationKey + ", 'contents': {'en': '" + message + "'} ,'include_player_ids': ['" + notificationKey + "'] }");
+
+        OneSignal.postNotification(jsonObject,
+                new OneSignal.PostNotificationResponseHandler() {
+                    @Override
+                    public void onSuccess(JSONObject response) {
+                        Log.i("OneSignalExample", "postNotification Success: " + response.toString());
+                    }
+
+                    @Override
+                    public void onFailure(JSONObject response) {
+                        Log.e("OneSignalExample", "postNotification Failure: " + response.toString());
+                    }
+                });
+    }
+}
+
